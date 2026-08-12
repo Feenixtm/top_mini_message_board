607ab9194ed32abe94827c69d23e762c4f9ecf37
feenixtm <177155245+Feenixtm@users.noreply.github.com>
2026-08-12 15:51:04 -0700

    Added a routes directory

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import router from "./routes/routes.js"
 
 const app = express();
 
 app.use(cors())
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({ message:"Hello there!" });
-})
+// app.get("/", (req, res) => {
+//     res.json();
+// })
+
+app.use("/", router);
 
 const PORT = process.env.PORT || 5051
 
